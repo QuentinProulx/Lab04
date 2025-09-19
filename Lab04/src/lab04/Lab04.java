8aa1de22ff4596e78a6e9f86d3601ce6f14d6202
Quentin Proulx <6324569@edu.vaniercollege.qc.ca>
2025-09-19 13:00:55 -0400

    First commit

---
+++ Lab04/src/lab04/Lab04.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab04;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 6324569
+ */
+public class Lab04 extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        
+        stage.setResizable(false);
+        stage.setTitle("Lab 04");
+        stage.show();
+    }
+    
+}
